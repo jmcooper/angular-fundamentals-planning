415a92862d5ec856238c545a85af50f298d1a67c
Jim Cooper <jim@cooperplanet.com>
2022-04-08 09:54:04 -0600

    Add Home route and Home banner image

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-fundamentals-planning';
+  title = `Joe's Robot Crap`;
 }
