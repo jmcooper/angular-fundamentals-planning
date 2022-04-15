51e37eba863d7ed32c666ffc239cf9e4a6666ec8
Joe Eames <joeeames@gmail.com>
2022-04-15 12:23:57 -0600

    added content projection

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,24 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { IProduct } from '../catalog/product.model';
 import { CartRepositoryService } from './cart-repository.service';
 
 @Component({
   selector: 'bot-cart',
   templateUrl: './cart.component.html',
-  styleUrls: ['./cart.component.css']
+  styleUrls: ['./cart.component.css'],
 })
 export class CartComponent {
+  constructor(private cartRepository: CartRepositoryService) {}
 
-  constructor(private cartRepository: CartRepositoryService) { }
-
-  get cartItems() { return this.cartRepository.cart; }
+  get cartItems() {
+    return this.cartRepository.cart;
+  }
 
   get cartTotal() {
-    return this.cartRepository.cart
-      .reduce((prev, next) => {
-        let discount = next.discount && next.discount > 0
-          ? 1 - next.discount
-          : 1;
-        return prev + (next.price * discount);
-      }, 0)
+    return this.cartRepository.cart.reduce((prev, next) => {
+      let discount = next.discount && next.discount > 0 ? 1 - next.discount : 1;
+      return prev + next.price * discount;
+    }, 0);
+  }
+
+  removeFromCart(product: IProduct) {
+    let cart = this.cartRepository.cart.filter((i) => i !== product);
+
+    this.cartRepository.saveCart(cart);
   }
 }
--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -1,27 +1,15 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { CartRepositoryService } from '../cart/cart-repository.service';
 import { IProduct } from '../catalog/product.model';
 
 @Component({
   selector: 'bot-product-details',
   templateUrl: './product-details.component.html',
-  styleUrls: ['./product-details.component.css']
+  styleUrls: ['./product-details.component.css'],
 })
 export class ProductDetailsComponent implements OnInit {
   @Input() product!: IProduct;
-  @Input() allowPurchase: boolean = false;
 
-  constructor(private cartRepository: CartRepositoryService) { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
-  addToCart(product: IProduct) {
-    let cart = [...this.cartRepository.cart, product];
-
-    this.cartRepository.saveCart(cart);
-  }
-
-
-
+  ngOnInit(): void {}
 }
