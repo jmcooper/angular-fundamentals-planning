085896dd6574250da7e51e0af205846a796ddb33
Jim Cooper <jim@cooperplanet.com>
2022-05-09 12:09:03 -0600

    Add Router Event handling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,39 @@
 import { Component } from '@angular/core';
+import {
+  NavigationCancel,
+  NavigationEnd,
+  NavigationError,
+  NavigationStart,
+  Router,
+  RouterEvent,
+} from '@angular/router';
+import { filter } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {}
+export class AppComponent {
+  loading: boolean = false;
+
+  constructor(private router: Router) {
+    router.events
+      .pipe(filter((event) => event instanceof RouterEvent))
+      .subscribe((routerEvent) => this.showOrHideLoadingMessage(routerEvent));
+  }
+
+  showOrHideLoadingMessage(routerEvent: any): void {
+    if (routerEvent instanceof NavigationStart) {
+      this.loading = true;
+    }
+
+    if (
+      routerEvent instanceof NavigationEnd ||
+      routerEvent instanceof NavigationCancel ||
+      routerEvent instanceof NavigationError
+    ) {
+      this.loading = false;
+    }
+  }
+}
